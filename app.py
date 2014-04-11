742a9a15621dd3e53871a19ba3b2f02d61fbc33e
changlinh <ch-@live.com>
2014-04-11 14:41:44 -0400

    Added foundation files

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world(name=None):
+    return render_template('index.html',name=name)
+
 @app.route("/weather")
 def get_weather():
     return flask.jsonify(current_cond = weather.get_weather())
@@ -10,6 +14,7 @@ def get_weather():
 @app.route("/food")
 def get_food():
     return flask.jsonify(food.get_food_all())
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
