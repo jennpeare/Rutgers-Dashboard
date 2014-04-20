c5aee95372dabc0f875694108fba79e7171af330
Jenny Shi <jennpeare@gmail.com>
2014-04-19 22:35:59 -0400

    played around with alternative display for index

--- app.py
+++ app.py
@@ -9,14 +9,23 @@ def index():
     return render_template("index.html", weather = weather.get_weather(), quote = quote.format_quote(quote.get_quote()))
     #return render_template("index.html", weather = "weather", quote = "quote")
 
+@app.route("/test.html")
+def test():
+    #return render_template("index.html", weather = weather.get_weather(), quote = quote.format_quote(quote.get_quote()))
+    return render_template("test.html",
+      weather = "weather",
+      quote = "quote",
+      food = food.get_food_master()
+      )
+
 @app.route("/weather")
 def get_weather():
     return flask.jsonify(weather.get_weather())
 
 @app.route("/food")
 def get_food():
-    return flask.jsonify(food.get_food_all())
-
+    #return flask.jsonify(food.get_food_all())
+    return flask.jsonify(food = food.get_food_master())
 @app.route("/quote")
 def get_quote():
     return flask.jsonify(quote.format_quote(quote.get_quote()))
--- food.py
+++ food.py
@@ -2,34 +2,72 @@ import requests, json
 from pprint import pprint
 
 foodURL = "https://rumobile.rutgers.edu/1/rutgers-dining.txt"
-dining_halls = {}
 
 def main():
     get_food_all()
-    get_food_dhall("Brower Commons", "Dinner")
+    get_food_dhall("Livingston Dining Commons", "Dinner")
+
+def get_food_master():
+    food = {}
+
+    busch = {}
+    busch["busch_breakfast"] = get_food_dhall("Busch Dining Hall", "Breakfast")
+    busch["busch_lunch"] = get_food_dhall("Busch Dining Hall", "Lunch")
+    busch["busch_dinner"] = get_food_dhall("Busch Dining Hall", "Dinner")
+    busch["busch_takeout"] = get_food_dhall("Busch Dining Hall", "Takeout")
+
+    livi = {}
+    livi["livi_breakfast"] = get_food_dhall("Livingston Dining Commons", "Breakfast")
+    livi["livi_lunch"] = get_food_dhall("Livingston Dining Commons", "Lunch")
+    livi["livi_dinner"] = get_food_dhall("Livingston Dining Commons", "Dinner")
+    livi["livi_takeout"] = get_food_dhall("Livingston Dining Commons", "Takeout")
+
+    brower = {}
+    busch["brower_breakfast"] = get_food_dhall("Brower Commons", "Breakfast")
+    busch["brower_lunch"] = get_food_dhall("Brower Commons", "Lunch")
+    busch["brower_dinner"] = get_food_dhall("Brower Commons", "Dinner")
+    busch["brower_takeout"] = get_food_dhall("Brower Commons", "Takeout")
+
+    neilson = {}
+    livi["neilson_breakfast"] = get_food_dhall("Neilson Dining Hall", "Breakfast")
+    livi["neilson_lunch"] = get_food_dhall("Neilson Dining Hall", "Lunch")
+    livi["neilson_dinner"] = get_food_dhall("Neilson Dining Hall", "Dinner")
+    livi["neilson_takeout"] = get_food_dhall("Neilson Dining Hall", "Takeout")
+
+    food["busch"] = busch
+    food["livi"] = livi
+    food["brower"] = busch
+    food["neilson"] = livi
+
+    return food
 
 def get_food_dhall(dhall, meal):
+
+    dining_halls = get_food_all()
+
     dhall = dining_halls.get(dhall)
     if dhall is not None:
         meal = dhall.get(meal)
     return meal
 
 def get_food_all():
+
     fooddata = requests.get(foodURL).json()
+    dining_halls = {}
 
     for location in fooddata:
-        #print location["location_name"]
         meal_name = {}
         for meals in location["meals"]:
-            #print "\t" + meals["meal_name"]
-            category = {}
+            category = []
             if meals["meal_avail"] == True:
                 for genres in meals["genres"]:
-                    #print "\t\t" + genres["genre_name"]
+                    genre = {}
+                    genre["name"] = genres["genre_name"]
                     food = [] # new food list for every genre
                     for item in genres["items"]:
                         food.append(item)
-                    category[genres["genre_name"]] = food
+                    genre["items"] = food
+                    category.append(genre)
                 meal_name[meals["meal_name"]] = category
         dining_halls[location["location_name"]] = meal_name
 
