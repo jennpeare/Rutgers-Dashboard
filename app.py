ed497e5f182ab1836451078fa7afc85d2572bbd6
Jenny Shi <jennpeare@gmail.com>
2014-04-20 17:02:35 -0400

    minor changes to app.py

--- app.py
+++ app.py
@@ -7,18 +7,15 @@ app = Flask(__name__)
 @app.route("/index.html")
 def index():
     return render_template("index.html", weather = weather.get_weather(), quote = quote.format_quote(quote.get_quote()))
-    #return render_template("index.html", weather = "weather", quote = "quote")
 
 @app.route("/test.html")
 def test():
-    #return render_template("index.html", weather = weather.get_weather(), quote = quote.format_quote(quote.get_quote()))
     return render_template("test.html",
       weather = weather.get_weather(),
       quote = quote.format_quote(quote.get_quote()),
       food = food.get_food_master(),
       bus_bcc = bus.get_predictions_for("Busch Campus Center"),
-      bus_hill = bus.get_predictions_for("Hill Center")
-      )
+      bus_hill = bus.get_predictions_for("Hill Center"))
 
 @app.route("/weather")
 def get_weather():
@@ -26,8 +23,8 @@ def get_weather():
 
 @app.route("/food")
 def get_food():
-    #return flask.jsonify(food.get_food_all())
     return flask.jsonify(food = food.get_food_master())
+
 @app.route("/quote")
 def get_quote():
     return flask.jsonify(quote.format_quote(quote.get_quote()))
